5722aa2e0469725decc7824e272eba29b36a7e7c
CaglarSahinler <caglar_sa@gmx.de>
2023-10-18 10:50:09 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -13,13 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hallo
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
           Ich bin {wert} Jahre alt und nicht {dieZahl()}.
         </p>
-        <p>Hallo</p>
+        <p>Hallo Welt</p>
         <a
           className="App-link"
           href="https://reactjs.org"
